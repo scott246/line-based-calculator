ce74d365bb2b41ff9f9873a7960432f5d44d36b8
Nathan Scott <scott246@purdue.edu>
2017-01-15 17:34:12 -0500

    Initial Commit

---
+++ Line Based Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Line Based Calculator
+//
+//  Created by Nathan Scott on 1/15/17.
+//  Copyright © 2017 Nathan Scott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
