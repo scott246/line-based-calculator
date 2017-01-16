9dd62d6f9ded323f3b5ea19b703c394b48a3eadb
Nathan Scott <scott246@purdue.edu>
2017-01-15 21:38:04 -0500

    beginning initial UI work

--- Line Based Calculator/ViewController.swift
+++ Line Based Calculator/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var input: UITextView!
+    @IBOutlet weak var output: UITextView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
